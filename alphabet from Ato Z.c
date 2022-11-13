df73a56b1cd23ce9d948146bd5745ecaa4942c95
Ravi Kumar <ravikumar_csd@srkrec.edu.in>
2022-11-13 21:41:37 +0530

    Alphabits order

---
+++ alphabet from Ato Z.c
@@ -0,0 +1,11 @@
+/*to print alphabet from Ato Z
+Author Ch Ravi Kumar Satya Sai Praveen
+Date :20 Feb 2022*/
+#include <stdio.h>
+int main()
+{
+    int i62;
+    for (i62 = 65; i62 <= 90; i62++)
+        printf("the number is %c\n", i62);
+    return 0;
+}
---
+++ alphabet from a to z.c
@@ -0,0 +1,11 @@
+/*to print alphabet from a to z
+Author Ch Ravi Kumar Satya Sai Praveen
+Date :20 Feb 2022*/
+#include <stdio.h>
+int main()
+{
+    int i62;
+    for (i62 = 97; i62 <= 122; i62++)
+        printf("the number is %c\n", i62);
+    return 0;
+}
